1f932b61f6784ca4b5ce0b163a0d2ead9d9c064c
Edwin <edwinjmenendez@gmail.com>
2020-06-05 20:52:53 -0700

    connect webpack; set up boilerplate;

---
+++ client/components/App.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import styles from '../../styles.css';
+
+const App = () => {
+  return <div>Pets</div>
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  mode: process.env.NODE_ENV,
+  devServer: {
+    publicPath: '/build',
+    proxy: {
+      '/api': 'http://localhost:3000'
+    },
+    hot: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+        },
+      },
+      {
+        test: /\.css/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+				test: /\.(png|svg|jpg|gif)$/,
+				use: [ 'file-loader', ],
+      },
+      {
+        test: /\.jpg?/,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              disable: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+}
