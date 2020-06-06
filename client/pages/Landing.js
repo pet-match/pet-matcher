fadf6e5fdb662d09fe6384aa77cbe23cbfd9aaff
Greg P <gregpalace>
2020-06-06 13:46:15 -0700

    Render Cards to Main Page

---
+++ client/pages/Landing.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Landing() {
+  return (
+    <di>
+      <h1>Hello</h1>
+    </di>
+  );
+}
---
+++ client/pages/Main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Deck from '../components/Deck';
+
+export default function Main() {
+  return (
+    <div>
+      <h1>Main Page</h1>
+      <Deck />
+    </div>
+  );
+}
