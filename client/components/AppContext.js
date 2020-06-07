2a86beadec375e8ceb633620da9eae39e4db132e
tdwolf6 <rodrickbuckner@gmail.com>
2020-06-06 19:08:17 -0700

    accept changes from frontend team

---
+++ client/components/AppContext.js
@@ -0,0 +1,22 @@
+import React, { useState, createContext } from 'react';
+
+// creates a global context for the entire app with state and setState.
+
+export const AppContext = createContext();
+
+export const AppProvider = (props) => {
+  const [state, setState] = useState({
+    users: [],
+    isLoggedIn: false,
+  });
+
+  return (
+    // Wrap all child components in our Provider tag allowing them,
+    // access to state:
+    <AppContext.Provider value={[state, setState]}>
+      {props.children}
+    </AppContext.Provider>
+  );
+};
+
+// state: {users:[], isLoggedIn: boolean}
--- client/pages/Main.js
+++ client/pages/Main.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
 import Deck from '../components/Deck';
+import { AppContext } from '../components/AppContext';
 
 export default function Main() {
+  const [state, setState] = useContext(AppContext);
+  const handleClick = () => setState({ isLoggedIn: !state.isLoggedIn });
   return (
     <div>
-      <h1>Main Page</h1>
+      <h1 onClick={handleClick}>Main Page</h1>
       <Deck />
     </div>
   );
